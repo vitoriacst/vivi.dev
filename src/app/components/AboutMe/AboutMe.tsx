7cc5e00553700a9bd2ba59ef1b57889f634e79e2
vitoria.cst <vitoria.cst.holanda@gmail.com>
2025-04-13 15:54:43 -0300

    Add AboutMe component with translation support

---
+++ src/app/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,16 @@
+import { useTranslation } from "react-i18next";
+
+export default function AboutMe() {
+  const { t } = useTranslation('common')
+
+  return (
+    <section className="flex flex-col items-start w-full justify-start">
+    <img src="/logo.png" alt="Vitória Holanda" className="h-24 rounded-full" />
+    <div className="flex flex-col items-start">
+      <h1 className="text-2xl font-bold"> {t('greeting')}  {t('apresentation')}</h1>
+        <h2 className="text-xl font-semibold">{t('position')}</h2>
+        <p className="text-sm">{t('aboutMe')}</p>
+    </div>
+  </section>
+  );
+}
