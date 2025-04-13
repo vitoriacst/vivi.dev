08d581d270ca73c5b3ceeaa40ce7ba4d9ea78e26
vitoria.cst <vitoria.cst.holanda@gmail.com>
2025-04-13 18:28:47 -0300

    Update project cards in Home component with new links and technologies; enhance layout for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24">
       <div className="w-full flex flex-col md:flex-row gap-4">
-
-        {/* Coluna esquerda */}
         <div className="w-full md:min-w-1/2 flex flex-col gap-4">
           <AboutMe />
           <div className="flex flex-col gap-4 shadow-md rounded-md p-4 h-full">
@@ -23,10 +21,11 @@ export default function Home() {
         <div className="w-full md:min-w-1/2 flex flex-col gap-4">
           <div className="flex-col flex gap-4 shadow-md rounded-md p-4 h-full items-start justify-start">
           <h1 className="text-xl font-bold">{t('projects')}</h1>
-          <div className="flex items-center justify-center gap-4 flex-wrap">
-           <ProjectsCards title={t('projectsDescription.0.viviUi')} description={t('projectsDescription.0.viviUiDescription')} technologies='Next.js.Tailwind CSS.TypeScript' link={t('projectsDescription.0.link')} />
-            <ProjectsCards title={t('projectsDescription.0.giblhiFlix')} description={t('projectsDescription.0.giblhiFlixDescription')} technologies='Next.js.Tailwind CSS.TypeScript' link={t('projectsDescription.0.link')} />
-            <ProjectsCards title={t('projectsDescription.0.daaiProject')} description={t('projectsDescription.0.daaiDescription')} technologies='Next.js.Tailwind CSS.TypeScript' link={t('projectsDescription.0.link')} />
+          <div className="flex gap-4 flex-wrap">
+           <ProjectsCards title={t('projectsDescription.0.viviUi')} description={t('projectsDescription.0.viviUiDescription')} technologies='Next.js.Tailwind.TypeScript' link={t('projectsDescription.0.viviUiLink')} />
+            <ProjectsCards title={t('projectsDescription.0.giblhiFlix')} description={t('projectsDescription.0.giblhiFlixDescription')} technologies='TypeScript.Tailwind.Cypress' link={t('projectsDescription.0.giblhiFlixLink')} />
+            <ProjectsCards title={t('projectsDescription.0.daaiProject')} description={t('projectsDescription.0.daaiDescription')} technologies='TypeScript.Tailwind.Stencil' link={t('projectsDescription.0.daaiLink')} />
+            <ProjectsCards title={t('projectsDescription.0.beersApp')} description={t('projectsDescription.0.beersAppDescription')} technologies='JavaScript.Node.Jest' link={t('projectsDescription.0.beersAppLink')} />
           </div>
           </div>
         </div>
